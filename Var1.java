6ad8b52ca8034a02821e2ab31a73a739f166d2e6
Vaibhav <vhake45@gmail.com>
2026-01-07 20:35:36 +0530

    Varible code 1

---
+++ Var1.java
@@ -0,0 +1,24 @@
+//Declar and then assign Value in Variable 
+class Var1{
+   public static void main(String []args){
+	   String name;
+	   byte age;
+	   String degree;
+	   int sal;
+	   long mob;
+	   float cgpa;
+	   
+	   name="Vaibhav Hake";
+	   age=22;
+	   degree="Btech";
+	   sal=5000000;
+	   mob=9970029150l;
+	   cgpa=7.85f;
+	   System.out.println(name);
+	   System.out.println(age);
+	   System.out.println(sal);
+	   System.out.println(degree);
+	   System.out.println(mob);
+	   System.out.println(cgpa);
+   }
+}
