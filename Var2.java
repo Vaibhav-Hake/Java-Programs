25f77571b2962c91a0103859d114edae9a1e02e1
Vaibhav <vhake45@gmail.com>
2026-01-07 20:36:25 +0530

    Varible 2 code

---
+++ Var2.java
@@ -0,0 +1,17 @@
+// Declar and Initilization Value in  Variable 
+class Var2{
+   public static void main(String []args){
+	   String name="Vaibhav Hake";
+	   byte age=22;
+	   String degree="Btech";
+	   int sal=5000000;
+	   long mob=9970029150l;
+	   float cgpa=7.85f;
+	   System.out.println(name);
+	   System.out.println(age);
+	   System.out.println(sal);
+	   System.out.println(degree);
+	   System.out.println(mob);
+	   System.out.println(cgpa);
+   }
+}
