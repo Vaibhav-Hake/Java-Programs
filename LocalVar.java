57ee3dfcaa17074a8c134439d4fa20f178eb92ff
Vaibhav-Hake <vhake45@gmail.com>
2026-01-08 19:28:44 +0530

    Update LocalVar.java

--- LocalVar.java
+++ LocalVar.java
@@ -6,3 +6,9 @@ class LocalVar{
    }
  }
 }
+
+/* Charecterstics Of Local varible 
+1) a varible declare inside block it will remain local to that block .
+2) We can not give local varible same name within same block . it will give compileTile Error .
+3) we can give local varible name can give same another varible name in diffrent block.
+4) Without initilization we can not use local varible . because local varible not give default val.*/ 
