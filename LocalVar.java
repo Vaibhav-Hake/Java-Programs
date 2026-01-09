3d238ea406438a7e55ba59c84c69f2dcec70de91
Vaibhav <vhake45@gmail.com>
2026-01-09 20:45:19 +0530

    Upade Local var

--- LocalVar.java
+++ LocalVar.java
@@ -3,6 +3,45 @@ class LocalVar{
    {
      int a=10;
 	 System.out.println(a);
+	  // not use give CTE
+	 // char a='v';
    }
  }
 }
+/* Charecterstics Of Local varible 
+1) a varible declare inside block it will remain local to that block .
+2) We can not give local varible same name within same block . it will give compileTile Error .
+3) we can give local varible name can give same another varible name in diffrent block.
+4) Without initilization we can not use local varible . because local varible not give default val.*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
