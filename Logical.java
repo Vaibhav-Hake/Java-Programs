72699de15471b6085ee75380809b201bbbfbfee4
Vaibhav <vhake45@gmail.com>
2026-01-17 00:03:47 +0530

    add optimized solution

--- Logical.java
+++ Logical.java
@@ -1,12 +1,12 @@
 class Logical{
 	public static void main(String[]args){
-		System.out.println(true&&false);
-		System.out.println(5%2==1&&9%3==0);
-		System.out.println('a'!='A' || false);
-		System.out.println(!false);
-		System.out.println(!true==false);
-		System.out.println(!false&&!true);
-		System.out.println(!false || !true);
-		System.out.println('a'<'A'||'A'<'a');
+		System.out.println(true&&false);//false 
+		System.out.println(5%2==1&&9%3==0);//true
+		System.out.println('a'!='A' || false);//true
+		System.out.println(!false);//true
+		System.out.println(!true==false);//true
+		System.out.println(!false&&!true);//false
+		System.out.println(!false || !true);//true
+		System.out.println('a'<'A'||'A'<'a');//true
 	}
 }
