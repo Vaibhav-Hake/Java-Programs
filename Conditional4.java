3af1dd0d82a5da328550d603046ef844110023e8
Vaibhav <vhake45@gmail.com>
2026-01-16 22:51:33 +0530

    Lerning Logical Opertor

--- Conditional4.java
+++ Conditional4.java
@@ -7,3 +7,8 @@ class Conditional4{
 		System.out.println(ans);
 	}
 }
+
+
+
+
+
---
+++ Logical.java
@@ -0,0 +1,12 @@
+class Logical{
+	public static void main(String[]args){
+		System.out.println(true&&false);
+		System.out.println(5%2==1&&9%3==0);
+		System.out.println('a'!='A' || false);
+		System.out.println(!false);
+		System.out.println(!true==false);
+		System.out.println(!false&&!true);
+		System.out.println(!false || !true);
+		System.out.println('a'<'A'||'A'<'a');
+	}
+}
