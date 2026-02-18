2122030588d50f0463bf37be435018b8c3bf50ce
Vaibhav <vhake45@gmail.com>
2026-02-18 11:27:38 +0530

    modify

--- Print.java
+++ Print.java
@@ -1,10 +1,16 @@
 class Print{
 	public static void main (String []args)throws InterruptedException{
 		System.out.println();
+		int duration=200;
+		int cnt=0;
 		for(char ch='A';ch<='Z';ch++){
 			System.out.print(ch);
-			Thread.sleep(2000);
-		}
+			duration+=100;
+			cnt++;
+			if(cnt%6==0)duration=100;
+			
+			Thread.sleep(duration);
+		 }
 		System.out.println();
 	}
 }
