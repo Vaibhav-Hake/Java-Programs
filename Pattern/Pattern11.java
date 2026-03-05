fe7ad7cc7d17913a9c8c58b38b8230f8464f04bd
Vaibhav <vhake45@gmail.com>
2026-03-05 23:25:28 +0530

    Pattern Program

---
+++ Pattern/Pattern11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Pattern11{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++){
+			for(int j=0;j<=n-i;j++){
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+		//Anthor Approach!
+		/*for(int i=n;i>0;i--){
+			for(int j=i;j>0;j--){
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}*/
+	}
+}
