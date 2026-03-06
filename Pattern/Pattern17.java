c65f2ea5cb080dadba47fae9e9750e22b4f06d9c
Vaibhav <vhake45@gmail.com>
2026-03-06 22:47:44 +0530

    Pattern Program

---
+++ Pattern/Pattern17.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Pattern17{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++){
+			for(int j=0;j<2*n-i-1;j++){
+				if(j>=i){
+					System.out.print("*"+" ");
+				}else{
+					System.out.print(" "+" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
