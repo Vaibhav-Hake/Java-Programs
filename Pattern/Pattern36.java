7ce7cc8d2bdbc3d7a4bdba7ea1b8db66914d6ebe
Vaibhav <vhake45@gmail.com>
2026-03-10 23:32:26 +0530

    Pattern program'

---
+++ Pattern/Pattern36.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Pattern36{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int space=1;
+		int star=n/2;
+		for(int i=0;i<n/2;i++){
+			for(int j=0;j<space;j++){
+				System.out.print(" "+" ");
+			}
+			for(int j=0;j<star;j++){
+				System.out.print("*"+"   ");
+			}
+			System.out.println();
+			if(i<n/2){
+				space++;
+				star--;
+			}
+		}
+	}
+}
