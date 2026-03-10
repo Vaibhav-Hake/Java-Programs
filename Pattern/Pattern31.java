e628f1e3ef944106835fcc49cd8a537c985c3305
Vaibhav <vhake45@gmail.com>
2026-03-10 11:34:35 +0530

    pattern program

---
+++ Pattern/Pattern31.java
@@ -0,0 +1,21 @@
+import java .util.Scanner;
+class Pattern31{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		int n= sc.nextInt();
+		int space=n/2 , star=1;
+		for(int i=0;i<n/2;i++){
+			for(int j=0;j<space;j++){
+				System.out.print(" "+" ");
+			}
+			for(int j=0;j<star;j++){
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+			if(i<n/2){
+				space--;
+				star+=2;
+			}
+		}
+	}
+}
