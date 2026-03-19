35a0be38decd892890c6b732be049f0e63bc7df4
Vaibhav <vhake45@gmail.com>
2026-03-19 23:51:55 +0530

    File Wraped

--- Methode/SumOfDigit.java
+++ Methode/SumOfDigit.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
-class Factorial{
-	public static void Factorial(int num){
-         int fact=1; 
+class SumOfDigit{
+	public static void sum(int num){
+		int sum=0;
 		while(num!=0){
-			fact*=num;
-			num--;
+			int rem=num%10;
+			sum+=rem;
+			num/=10;
 		}
-		System.out.println(fact);
-		
+		System.out.println(sum);
 		
 	}
 	public static void main(String[]args){
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter a Number :");
 		int num=sc.nextInt();
-		Factorial(num);
-		
+		sum(num);
 	}
 }
