dfc4aabd725ba5b0e2c7afa0ac04f3aad6e28944
Vaibhav <vhake45@gmail.com>
2026-04-03 00:02:22 +0530

    Factorial recursive

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 class Factorial{
 	public static int fact(int num){
+		if(num==0){
+			return 1;
+		}
+		return num*fact(num-1);
 		
-		
-		return num==0 ? 1:num*fact(num-1);
+		//return num==0 ? 1:num*fact(num-1);
 	}
 	public static void main(String[]args){
 		System.out.println(fact(5));
