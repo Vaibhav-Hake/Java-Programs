aba1545100e4bef00877b5e0d8b1b3f84f58cc4f
Vaibhav <vhake45@gmail.com>
2026-04-02 23:22:27 +0530

    power using recursive

---
+++ Recursion/Power.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Power{
+	public static int pow(int base,int raise){
+		if(raise==0){
+			return 1;
+			
+		}
+		return base* pow(base,raise-1);
+	}
+	public static void main(String[] args){
+		System.out.println(pow(2,3));
+	}
+}
