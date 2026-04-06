1e855cc623c0c042f2d6809b521290d84823cd30
Vaibhav <vhake45@gmail.com>
2026-04-06 10:56:44 +0530

    Modified code for gcd

--- Recursion/GcdSumOddEven.java
+++ Recursion/GcdSumOddEven.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 class GcdSumOddSumEven{
 	
 	// fully optimized 
-	public static int gcd(int n){
+	/*public static int gcd(int n){
 		return n;
 	}
 	public static void main(String[]args){
 		System.out.println(gcd(5));
-	}
+	}*/
 	
 	
 	// optimize not till
-	/* public int gcdOfOddEvenSums(int n) {
+	 public int gcdOfOddEvenSums(int n) {
         //return n;
          int sumOdd=n*n;
          int sumEven=n*(n+1);
@@ -28,5 +28,5 @@ class GcdSumOddSumEven{
          }
          return small;
 
-     }*/
-}+
+     }
+}
