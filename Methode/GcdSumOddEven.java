5a7ebfb5d646d5510366bd1eaf28434b362fd3a5
Vaibhav <vhake45@gmail.com>
2026-04-06 11:33:07 +0530

    This is try a referance class for static methode

---
+++ Methode/GcdSumOddEven.java
@@ -0,0 +1,9 @@
+class GcdSumOddEven{
+
+	public static void main(String[]args){
+		int n=10;
+		int oddSum=n*n;
+		int evenSum=n*(n+1);
+		System.out.println(Hcf.gcd(oddSum,evenSum));
+	}
+}    //in this we are add a referance class  
