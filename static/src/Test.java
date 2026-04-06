918bd1d7481fb7c8cbedfd8fca2cf8ea700c2747
Vaibhav <vhake45@gmail.com>
2026-04-06 23:09:48 +0530

    Static Varible

---
+++ static/src/Test.java
@@ -0,0 +1,11 @@
+class Test{
+	static int a;
+	public static void main(String[]args){
+		System.out.println("main methode");
+		System.out.println(a);
+		demo();
+	}
+	public static void demo(){
+		System.out.println(a);
+	}
+}
