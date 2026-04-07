b9d08c2fca5dfec842562f48a4dba0a5fcc8eb42
Vaibhav <vhake45@gmail.com>
2026-04-07 13:23:40 +0530

    the test a static varible

---
+++ static/src/Test3.java
@@ -0,0 +1,12 @@
+class Test3{
+	static int a;
+	 public static void main(String[]args){
+		int a=10;
+		 System.out.println(a);
+		 demo();
+	 }
+	 public static void demo(){
+		//int a=52;
+		 System.out.println(a);
+	 }
+}
