b6ca135398d75049efce15d1c5db587bd10def37
Vaibhav <vhake45@gmail.com>
2026-04-09 11:32:24 +0530

    test static

---
+++ static/src/Test5.java
@@ -0,0 +1,7 @@
+class Test5{
+	static int a=Test6.a;
+	public static void main(String[]args){
+		System.out.println(a);
+		System.out.println(Test6.a);
+	}
+}
---
+++ static/src/Test6.java
@@ -0,0 +1,7 @@
+class Test6{
+	static int a=Test5.a;
+	public static void main(String[]args){
+		System.out.println(a);
+		System.out.println(Test5.a);
+	}
+}
