39d0f99d4ae73b708d54e8827ce5fcd31086b252
Vaibhav <vhake45@gmail.com>
2026-05-03 14:31:43 +0530

    Byte Example

---
+++ Basic/Calender.java
@@ -0,0 +1,15 @@
+class Calender{
+   public static void main(String[]args){
+	   String name="Calender";
+	   short dayOfYear=365;
+	   short hourYear=(short)(dayOfYear*60);
+	   short minYear=(short)(dayOfYear*hourYear*60);
+	   
+	   System.out.println(dayOfYear);
+	   System.out.println(hourYear);
+	    System.out.println(minYear);
+		 
+		  System.out.println(hourYear);
+	   
+   }
+}
