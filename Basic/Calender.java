9127e90bad1f457e050c9ddcf186365c0218c369
Vaibhav <vhake45@gmail.com>
2026-05-08 12:29:53 +0530

    Operator program

--- Basic/Calender.java
+++ Basic/Calender.java
@@ -1,15 +1,29 @@
 class Calender{
    public static void main(String[]args){
-	   String name="Calender";
-	   short dayOfYear=365;
-	   short hourYear=(short)(dayOfYear*60);
-	   short minYear=(short)(dayOfYear*hourYear*60);
+	   String title="Calender";
 	   
-	   System.out.println(dayOfYear);
-	   System.out.println(hourYear);
-	    System.out.println(minYear);
-		 
-		  System.out.println(hourYear);
+	   int day=365;
+	   int noHour=day*24;
+	   int noMiniute=noHour*60;
+	   int noSec=noMiniute*60;
+	   int noHourMonth=30*60;
+	   int noMinMonth=noHourMonth*60;
+	   int noSecMonth=noMinMonth*60;
+	   
+	   System.out.println(title);
+	   System.out.println("No of Day In Year :"+day);
+
+	   System.out.println("No of Hour in Year :"+noHour);
+
+	   System.out.println("No of Miniute in Year :"+noMiniute);
+
+	   System.out.println("No of Second in Year :"+noSec);
+
+	   System.out.println("No of Hour in Month :"+noHourMonth);
+
+	   System.out.println("No of Miniute in Month :"+noMinMonth);
+	   System.out.println("No of Second in Month :"+noSecMonth);
+
 	   
    }
 }
---
+++ Basic/Q34.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Q34{
+   public static void main(String[]args){
+	   Scanner sc=new Scanner(System.in);
+	  double a=sc.nextInt();
+	  
+	  double b=sc.nextInt();
+	 
+	  double c=a+b;
+	  System.out.println(c);
+	   
+   }
+}
